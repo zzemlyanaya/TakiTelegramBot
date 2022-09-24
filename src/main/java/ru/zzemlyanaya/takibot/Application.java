534ac7b8ff264d3680e26577a6e186c7ba158ed5
zzemlyanaya <zem.jane@gmail.com>
2022-09-24 16:48:43 +0500

    init commit

---
+++ src/main/java/ru/zzemlyanaya/takibot/Application.java
@@ -0,0 +1,9 @@
+package ru.zzemlyanaya.takibot;
+
+/* created by zzemlyanaya on 24/9/2022 */
+
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
